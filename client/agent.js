f562fa176a72fb19c6bffbcd6b4ca8cd91e2a146
Julie Ng <me@julie.io>
2018-09-15 13:26:25 +0200

    chore: add link to https.Agent documentation

--- client/agent.js
+++ client/agent.js
@@ -2,6 +2,9 @@ const https = require('https');
 const fs = require('fs');
 const path = require('path');
 
+// For more `https.Agent` options, see here:
+// https://nodejs.org/api/https.html#https_https_request_options_callback
+
 module.exports = function (name) {
 	const certFile = path.resolve(__dirname, `${name}_cert.pem`);
 	const keyFile = path.resolve(__dirname, `${name}_key.pem`);
